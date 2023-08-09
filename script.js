453e45fd1f8f7bccc35054c2ae86b0f6a533da2f
BaQuna <imbaquna@gmail.com>
2023-08-09 15:38:35 +0600

    todo list

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const list = document.querySelector('#list-container')
 
 function addTask() {
     if (input.value === ''){
-        alert('EMPTY')
+        alert('Empty !')
     }
     else{
         let li = document.createElement('li')
-        li.innerHTML = input.value
         list.appendChild(li)
+        const data = new Date()
+        const hours = data.getHours().toString().padStart(2 , '0')
+        const minutes = data.getMinutes().toString().padStart(2 , '0')
+        const time = `${hours}:${minutes}`
+        li.innerHTML = `${input.value}.   (${time})`
         let span = document.createElement('span')
         span.innerHTML = '\u00d7'
         li.appendChild(span)
